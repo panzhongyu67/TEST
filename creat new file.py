063e7b6e39607616ffa0013978f399f923ec88ca
panzhongyu67 <37795133+panzhongyu67@users.noreply.github.com>
2018-04-13 10:29:20 +0800

    Update creat new file.py

--- creat new file.py
+++ creat new file.py
@@ -2,4 +2,5 @@ Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)]
 Type "copyright", "credits" or "license()" for more information.
 >>> print("上传新文件")
 上传新文件
->>> 
+>>> print（“第一次修改文件”）
+第一次修改文件
